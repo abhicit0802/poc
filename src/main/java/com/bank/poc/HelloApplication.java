4a9fccba9c79d1809eb389c565c28a8cdba88b3a
Abhinav Tiwary <abhicit0802@yahoo.com>
2018-09-13 21:02:44 +0530

    Access on k8s initial commit

---
+++ src/main/java/com/bank/poc/HelloApplication.java
@@ -0,0 +1,13 @@
+package com.bank.poc;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        run(HelloApplication.class, args);
+    }
+}
